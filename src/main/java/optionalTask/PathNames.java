262631d5ad644e57c129ca613aa767f9b0e6d2da
Alena Korneva <ekorneva323@gmail.com>
2022-02-13 19:20:37 +0300

    pass common ArrayLists to each method which apply it; save paths to static variable in separate class

---
+++ src/main/java/optionalTask/PathNames.java
@@ -0,0 +1,7 @@
+package optionalTask;
+
+public class PathNames {
+    protected static final String fileToWriteRandomNumbers = "D:\\TasksFromEpamCourses\\IOFundamentals\\src\\main\\resources\\fileToWriteRandomNumbers.txt";
+    protected static final String javaProgramForTask2 = "D:\\TasksFromEpamCourses\\IOFundamentals\\src\\main\\resources\\javaProgramForTask2.txt";
+    protected static final String javaProgramForTask3 = "D:\\TasksFromEpamCourses\\IOFundamentals\\src\\main\\resources\\javaProgramForTask3.txt";
+}
