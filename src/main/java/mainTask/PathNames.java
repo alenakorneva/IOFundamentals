d324f857dcd36ae6f4fa0489a9e5593a58dad76b
Alena Korneva <ekorneva323@gmail.com>
2022-03-24 13:31:15 +0300

    add all finished working tasks

---
+++ src/main/java/mainTask/PathNames.java
@@ -0,0 +1,5 @@
+package mainTask;
+
+public class PathNames {
+    static final String treeDerectoriesFromMainTask = "D:\\TasksFromEpamCourses\\IOFundamentals\\src\\main\\resources\\treeDirectoriesFromMainTask.txt";
+}
